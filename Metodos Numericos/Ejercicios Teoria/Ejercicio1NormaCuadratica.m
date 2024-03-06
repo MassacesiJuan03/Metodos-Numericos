dac8ba73f0c5d3371f6f92cd34fe70f102a8b990
Juan Ignacio <111523496+MassacesiJuan03@users.noreply.github.com>
2024-03-06 12:24:57 -0300

    Add files via upload

---
+++ Metodos Numericos/Ejercicios Teoria/Ejercicio1NormaCuadratica.m
@@ -0,0 +1,12 @@
+function normaCuadratica
+  % Elevamos al cuadrado las componentes y sumamos
+  sumaCuadratica = 0
+  for i=1:5
+    disp("Ingrese un componente")
+    vector(i) = input("");
+    sumaCuadratica += (vector(i)^2);
+  endfor
+  % Calculamos la norma cuadratica
+  disp("La norma cuadratica es:");
+  disp(sqrt(sumaCuadratica));
+ end
---
+++ Metodos Numericos/Ejercicios Teoria/Ejercicio2NormaInfinita.m
@@ -0,0 +1,17 @@
+function normaInfinita
+  %Ingresar componentes del vector
+  for i=1:5
+    disp("Ingrese una componente al vector")
+    vector(i) = input('');
+  endfor
+  % Obtener la norma infinita del vector
+  j = 1;
+  maxComponent = abs(vector(1))
+  while (j<=5)
+    if abs(vector(j)) >= maxComponent
+      maxComponent = abs(vector(j));
+    endif
+    j += 1;
+  endwhile
+  disp("La norma infinita del vector es: "), maxComponent
+end
